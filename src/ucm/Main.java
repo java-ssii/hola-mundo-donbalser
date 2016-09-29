da0daa38536a3e788d7fc738e7629f876759cbdc
donbalser <yiwohotu@cartelera.org>
2016-09-29 14:46:13 +0200

    Now interprets command-line args

--- src/ucm/Main.java
+++ src/ucm/Main.java
@@ -3,7 +3,11 @@ package ucm;
 public class Main {
 
 	public static void main(String ... args) {
-		System.out.println("Hello world!");
+		if (args.length > 0) {
+			System.out.println("Hi, " + args[0] + "!");
+		} else {
+			System.out.println("Hello world!");
+		}
 	}
 }
 
